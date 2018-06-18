39c3f4501caba08a22af1f1221a67d8c64059008
Bohdan Nikolaienko <23bohdan@gmail.com>
2018-06-18 23:52:37 +0300

    Update socket tests

--- test/in_touch_web/channels/user_channel_test.exs
+++ test/in_touch_web/channels/user_channel_test.exs
@@ -2,23 +2,22 @@ defmodule InTouchWeb.UserChannelTest do
   use InTouchWeb.ChannelCase
 
   alias InTouchWeb.UserChannel
+  alias InTouch.Identification
+
+  @user_attrs %{name: "some name", password: "some password"}
+
+  def fixture(:user) do
+    {:ok, user} = Identification.create_user(@user_attrs)
+    user
+  end
 
   setup do
+    user = fixture(:user)
     {:ok, _, socket} =
-      socket("user_id", %{some: :assign})
-      |> subscribe_and_join(UserChannel, "user:lobby")
+      socket("user_sosket:#{user.id}", %{user_id: user.id})
+      |> subscribe_and_join(UserChannel, "user:#{user.id}")
 
     {:ok, socket: socket}
-  end
-
-  test "ping replies with status ok", %{socket: socket} do
-    ref = push socket, "ping", %{"hello" => "there"}
-    assert_reply ref, :ok, %{"hello" => "there"}
-  end
-
-  test "shout broadcasts to user:lobby", %{socket: socket} do
-    push socket, "shout", %{"hello" => "all"}
-    assert_broadcast "shout", %{"hello" => "all"}
   end
 
   test "broadcasts are pushed to the client", %{socket: socket} do
